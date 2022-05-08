9bdd5f4d235e4ffba775de50a125fe6f4ec9e158
Roshith R <r.roshith@gmail.com>
2022-05-08 12:18:02 +0530

    Adding sanity to the project

---
+++ sanity_backend/schemas/test.js
@@ -0,0 +1,23 @@
+
+export default {
+ name: 'test',
+ title: 'Test 1',
+ type: 'document',
+ fields: [
+   {
+     name: 'name',
+     type: 'string',
+     title: 'Name'
+   },
+   {
+    name: 'age',
+    type: 'string',
+    title: 'Age'
+  },
+  {
+   name: 'sex',
+   type: 'boolean',
+   title: 'Sex'
+ }
+ ]
+}
