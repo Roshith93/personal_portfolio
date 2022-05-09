98df01e3158085f0df54e5bd671703703b9f2f2e
Roshith R <r.roshith@gmail.com>
2022-05-09 07:59:13 +0530

    Added components needed for the Project

--- react_frontend/src/App.js
+++ react_frontend/src/App.js
@@ -1,8 +1,22 @@
 import React from 'react'
 
+import { About } from '../src/container'
+import { Footer } from '../src/container'
+import { Header } from '../src/container'
+import { Skills } from '../src/container'
+import { Testimonial } from '../src/container'
+import { Work } from '../src/container'
+
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <About />
+      <Footer />
+      <Header />
+      <Testimonial />
+      <Skills />
+      <Work />
+    </div>
   )
 }
 
